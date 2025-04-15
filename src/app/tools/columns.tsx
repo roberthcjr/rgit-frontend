3d19635d48235567431904b253ee014fac15ba2c
Robert Carvalho <robert.carvalho@educare.digital>
2025-04-14 21:58:40 -0300

    feat: tools table working progress

---
+++ src/app/tools/columns.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+
+export type Tools = {
+    id: string
+    name: string
+    status: "available" | "unavailable" | "lended"
+    brand: string
+    category?: string
+    insertedAt?: Date
+  }
+
+export const columns: ColumnDef<Tools>[] = [
+    {
+        accessorKey: "name",
+        header: "Nome",
+    },
+    {
+        accessorKey: "category",
+        header: "Categoria",
+    },
+    {
+        accessorKey: "brand",
+        header: "Marca",
+    },
+    {
+        accessorKey: "insertedAt",
+        header: "Compra",
+    },
+    {
+      accessorKey: "status",
+      header: "Status",
+    },
+]
