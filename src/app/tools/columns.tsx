6fdb20fa6f93a57807196a626c1154df6760fe6c
Robert Carvalho <robert.carvalho@educare.digital>
2025-04-16 20:55:19 -0300

    feat: adds sorting method asc

--- src/app/tools/columns.tsx
+++ src/app/tools/columns.tsx
@@ -1,35 +1,95 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
 import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown } from "lucide-react"
 
 export type Tools = {
     id: string
     name: string
-    status: "available" | "unavailable" | "lended"
+    status: Status
     brand: string
     category?: string
     insertedAt?: string
   }
 
+export enum Status {
+    available = "Disponível", 
+    unavailable =  "Indisponível",
+    lended =  "Emprestada"
+}
+
+
+
 export const columns: ColumnDef<Tools>[] = [
     {
         accessorKey: "name",
-        header: "Nome",
+        header: ({ column }) => {
+            return (
+              <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+              >
+                Nome
+                <ArrowUpDown />
+              </Button>
+            )
+          },
     },
     {
         accessorKey: "category",
-        header: "Categoria",
+        header: ({ column }) => {
+            return (
+              <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+              >
+                Categoria
+                <ArrowUpDown />
+              </Button>
+            )
+          },
     },
     {
         accessorKey: "brand",
-        header: "Marca",
+        header: ({ column }) => {
+            return (
+              <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+              >
+                Marca
+                <ArrowUpDown />
+              </Button>
+            )
+          },
     },
     {
         accessorKey: "insertedAt",
-        header: "Compra",
+        header: ({ column }) => {
+            return (
+              <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+              >
+                Data de Compra
+                <ArrowUpDown />
+              </Button>
+            )
+          },
     },
     {
       accessorKey: "status",
-      header: "Status",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Status
+            <ArrowUpDown />
+          </Button>
+        )
+      },
     },
 ]
