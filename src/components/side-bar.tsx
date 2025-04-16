db7d954e17c7a3fbab317394af66e4b41ebf0f11
Robert Carvalho <robert.carvalho@educare.digital>
2025-04-16 20:57:23 -0300

    feat: finalizes side bar style

--- src/components/side-bar.tsx
+++ src/components/side-bar.tsx
@@ -1,51 +1,100 @@
+"use client";
+
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import { ArrowRightLeft, Wrench } from "lucide-react";
+import { ArrowRightLeft, ChevronUp, User2, Wrench } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { useState } from "react";
 
 // Menu items.
-const items = [
-    {
-      title: "Empréstimos",
-      url: "#",
-      icon: ArrowRightLeft,
-    },
-    {
-      title: "Ferramentas",
-      url: "#",
-      icon: Wrench,
-    },
-  ]
+const groupsSideBar = [
+  {
+    title: "Ferramentas",
+    items: [{ title: "Empréstimos", url: "#", icon: ArrowRightLeft }],
+  },
+  {
+    title: "Gerenciamento",
+    items: [{ title: "Ferramentas", url: "/tools", icon: Wrench }],
+  },
+];
 
 export function AppSidebar() {
+  const [selectedItem, setSelectedItem] = useState("");
+  const updateSelectedItem = (item: any) => {
+    console.log(item);
+    setSelectedItem(item);
+  }
   return (
     <Sidebar>
+      <SidebarHeader className="font-bold">RGIT</SidebarHeader>
       <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>RGIT</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+        {groupsSideBar.map((group) => (
+          <SidebarGroup key={group.title}>
+            <SidebarGroupLabel>{group.title}</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {group.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton
+                      asChild
+                      onClick={() => {updateSelectedItem(item.title)}}
+                      isActive={selectedItem===item.title}
+                    >
+                      <a href={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
       </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> Username
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width]"
+              >
+                <DropdownMenuItem>
+                  <span>Account</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Billing</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Sign out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 }
