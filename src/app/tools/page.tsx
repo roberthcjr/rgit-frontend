2eac273f06e3410fc7f6d73e8c35fb568cc414d6
Robert Carvalho <robert.carvalho@educare.digital>
2025-04-16 21:09:11 -0300

    feat: isolates dialog component

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -3,22 +3,119 @@
 import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
 import { Button } from "@/components/ui/button";
-import { CirclePlus } from "lucide-react";
+import { CirclePlus, PackageCheck, PackageX } from "lucide-react";
 import { tools } from "./mocks/tools.mock";
 import { CustomDialog } from "@/components/custom-dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const MB_RATIO = 1024 * 1024;
+const MAX_FILE_SIZE = 5 * MB_RATIO;
+const ACCEPTED_MIME_TYPES = ["text/csv"];
+
+const formSchema = z.object({
+  csv: z
+    .any()
+    .refine(
+      (file) => file?.size <= MAX_FILE_SIZE,
+      `O arquivo deve ter até ${MAX_FILE_SIZE / MB_RATIO}MB.`
+    )
+    .refine(
+      (file) => ACCEPTED_MIME_TYPES.includes(file?.type),
+      `Apenas arquivos no formato ${ACCEPTED_MIME_TYPES.join(
+        ","
+      )} são suportados.`
+    ),
+});
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      csv: "",
+    },
+  });
 
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      throw new Error("Isso é um erro");
+      // Send to api csv process
+      console.log(values);
+      toast("Importação realizada com sucesso!", {
+        icon: <PackageCheck />,
+      });
+      setOpen(false);
+    } catch (error) {
+      toast("Importação não pode ser realizada!", {
+        icon: <PackageX />,
+      });
+    }
+  }
   return (
     <div className="container">
       <div className="container mx-auto pt-10 pr-5">
         <DataTable columns={columns} data={tools} />
       </div>
 
-
       <div className="flex items-end">
-        <CustomDialog/>
+        <CustomDialog
+          trigger={
+            <Button className="cursor-pointer m-2" variant="outline">
+              Importar por CSV
+            </Button>
+          }
+          title="Importar CSV"
+        >
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="flex flex-col gap-4 mt-4"
+            >
+              <FormField
+                control={form.control}
+                name="csv"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        className="p-20"
+                        type="file"
+                        accept="text/csv"
+                        onChange={(input) => {
+                          const file = input.target.files?.[0];
+                          if (file) {
+                            field.onChange(file);
+                          }
+                        }}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Envie um arquivo csv para inserir ferramentas em massa.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="pointer">
+                Importar
+              </Button>
+            </form>
+          </Form>
+        </CustomDialog>
         <Button className="cursor-pointer m-2" variant="default">
           <CirclePlus />
           Inserir
--- src/components/custom-dialog.tsx
+++ src/components/custom-dialog.tsx
@@ -5,120 +5,27 @@ import {
   DialogTitle,
   DialogDescription,
 } from "./ui/dialog";
-import { useForm } from "react-hook-form";
-import { Button } from "./ui/button";
 import { DialogHeader } from "./ui/dialog";
-import {
-  Form,
-  FormField,
-  FormItem,
-  FormControl,
-  FormDescription,
-  FormMessage,
-} from "./ui/form";
-import { Input } from "./ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { PackageCheck, PackageX } from "lucide-react";
-import { ReactNode, useState } from "react";
-import { toast } from "sonner";
-import { z } from "zod";
+import { ReactNode } from "react";
+import { DialogProps } from "@radix-ui/react-dialog";
 
-type CustomDialogProps = {
+interface CustomDialogProps extends DialogProps {
   children: ReactNode;
-  content: ReactNode;
+  trigger: ReactNode;
   title: string;  
 }
 
-const MB_RATIO = 1024 * 1024;
-const MAX_FILE_SIZE = 5 * MB_RATIO;
-const ACCEPTED_MIME_TYPES = ["text/csv"];
-
-const formSchema = z.object({
-  csv: z
-    .any()
-    .refine(
-      (file) => file?.size <= MAX_FILE_SIZE,
-      `O arquivo deve ter até ${MAX_FILE_SIZE / MB_RATIO}MB.`
-    )
-    .refine(
-      (file) => ACCEPTED_MIME_TYPES.includes(file?.type),
-      `Apenas arquivos no formato ${ACCEPTED_MIME_TYPES.join(
-        ","
-      )} são suportados.`
-    ),
-});
-
-export function CustomDialog() {
-  const [open, setOpen] = useState(false);
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      csv: "",
-    },
-  });
-
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    try {
-      throw new Error("Isso é um erro");
-      // Send to api csv process
-      console.log(values);
-      toast("Importação realizada com sucesso!", {
-        icon: <PackageCheck />,
-      });
-      setOpen(false);
-    } catch (error) {
-      toast("Importação não pode ser realizada!", {
-        icon: <PackageX />,
-      });
-    }
-  }
+export function CustomDialog({children, trigger, title, open, onOpenChange}: CustomDialogProps) {
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogTrigger asChild>
-        <Button className="cursor-pointer m-2" variant="outline">
-          Importar por CSV
-        </Button>
+        {trigger}
       </DialogTrigger>
       <DialogContent className="flex justify-center">
         <DialogHeader className="flex items-center justify-center">
-          <DialogTitle>Importar por CSV</DialogTitle>
+          <DialogTitle>{title}</DialogTitle>
           <DialogDescription asChild>
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="flex flex-col gap-4 mt-4"
-              >
-                <FormField
-                  control={form.control}
-                  name="csv"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input
-                          className="p-20"
-                          type="file"
-                          accept="text/csv"
-                          onChange={(input) => {
-                            const file = input.target.files?.[0];
-                            if (file) {
-                              field.onChange(file);
-                            }
-                          }}
-                        />
-                      </FormControl>
-                      <FormDescription>
-                        Envie um arquivo csv para inserir ferramentas em massa.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit" className="pointer">
-                  Importar
-                </Button>
-              </form>
-            </Form>
+            {children}
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
