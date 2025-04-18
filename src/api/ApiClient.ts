2af26e8e1a863d2addfbf8009acf58e0696a30df
Robert Carvalho <robert.carvalho@educare.digital>
2025-04-17 22:33:56 -0300

    feat(ApiClient): creates default api

---
+++ src/api/ApiClient.ts
@@ -0,0 +1,24 @@
+export default class ApiClient {
+    api: string = "http://localhost:8080";
+    headers: Headers = new Headers();
+
+    get(url: string) : Promise<Response> {
+        const getRequest = new Request(`${this.api}/${url}`, {method: "GET", headers: this.headers});
+        return fetch(getRequest);
+    }
+
+    post(url: string, body: any) : Promise<Response> {
+        const postRequest = new Request(`${this.api}/${url}`, {method: "POST", headers: this.headers, body});
+        return fetch(postRequest);
+    }
+
+    put(url: string, body: any) : Promise<Response> {
+        const putRequest = new Request(`${this.api}/${url}`, {method: "PUT", headers: this.headers, body});
+        return fetch(putRequest);
+    }
+
+    delete(url: string) : Promise<Response> {
+        const deleteRequest = new Request(`${this.api}/${url}`, {method: "DELETE", headers: this.headers});
+        return fetch(deleteRequest);
+    }
+}
