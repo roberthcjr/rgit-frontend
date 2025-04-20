1f7f12a24e99753d5cf7dbd371f4b7a2091bb6df
Robert Carvalho <robert.carvalho@educare.digital>
2025-04-20 16:41:35 -0300

    style: prettier formatting

--- src/api/ApiClient.ts
+++ src/api/ApiClient.ts
@@ -1,26 +1,40 @@
 export default class ApiClient {
-    api: string = process.env.API??"http://localhost:8080";
-    headers: Headers = new Headers({
-        "Content-Type": "application/json",
+  api: string = process.env.API ?? "http://localhost:8080";
+  headers: Headers = new Headers({
+    "Content-Type": "application/json",
+  });
+
+  get(url: string): Promise<Response> {
+    const getRequest = new Request(`${this.api}/${url}`, {
+      method: "GET",
+      headers: this.headers,
     });
+    return fetch(getRequest);
+  }
 
-    get(url: string) : Promise<Response> {
-        const getRequest = new Request(`${this.api}/${url}`, {method: "GET", headers: this.headers});
-        return fetch(getRequest);
-    }
+  post(url: string, body: any, customHeader?: Headers): Promise<Response> {
+    const postRequest = new Request(`${this.api}/${url}`, {
+      method: "POST",
+      headers: customHeader ?? this.headers,
+      body,
+    });
+    return fetch(postRequest);
+  }
 
-    post(url: string, body: any, customHeader?: Headers) : Promise<Response> {
-        const postRequest = new Request(`${this.api}/${url}`, {method: "POST", headers: customHeader??this.headers, body});
-        return fetch(postRequest);
-    }
+  put(url: string, body: any): Promise<Response> {
+    const putRequest = new Request(`${this.api}/${url}`, {
+      method: "PUT",
+      headers: this.headers,
+      body,
+    });
+    return fetch(putRequest);
+  }
 
-    put(url: string, body: any) : Promise<Response> {
-        const putRequest = new Request(`${this.api}/${url}`, {method: "PUT", headers: this.headers, body});
-        return fetch(putRequest);
-    }
-
-    delete(url: string) : Promise<Response> {
-        const deleteRequest = new Request(`${this.api}/${url}`, {method: "DELETE", headers: this.headers});
-        return fetch(deleteRequest);
-    }
+  delete(url: string): Promise<Response> {
+    const deleteRequest = new Request(`${this.api}/${url}`, {
+      method: "DELETE",
+      headers: this.headers,
+    });
+    return fetch(deleteRequest);
+  }
 }
