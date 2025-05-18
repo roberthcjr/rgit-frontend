5f21e1adb46b59955fe9fd1f81c625c833d3bd90
Robert Carvalho <robert.carvalho@educare.digital>
2025-05-18 12:08:48 -0300

    refactor: remove useless `ReactQuery` statements

--- src/app/(public)/(auth)/login/hooks/useLogin.ts
+++ src/app/(public)/(auth)/login/hooks/useLogin.ts
@@ -2,40 +2,14 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { LoginSchema } from "../validator/login.validator";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import AuthenticationService from "../service";
-import { useMemo } from "react";
-import { showErrorToast } from "../components/login-toaster";
-import { LoginType } from "../model";
-import { useRouter } from "next/navigation";
 
 export const useLogin = () => {
-  const queryClient = useQueryClient();
-  const authenticationService = useMemo(() => new AuthenticationService(), []);
-  const router = useRouter();
-
-  const mutation = useMutation({
-    mutationFn: (data: LoginType) => authenticationService.login(data),
-    onError: (error) => {
-      showErrorToast(error.message);
-    },
-    onSuccess: () => {
-      router.push('/tools')
-      queryClient.invalidateQueries({ queryKey: ["users"] });
-    },
-  });
-
   const form = useForm<z.infer<typeof LoginSchema>>({
     resolver: zodResolver(LoginSchema),
     defaultValues: {
       username: "",
-      password: ""
+      password: "",
     },
   });
-  
-  const onSubmit = (data: z.infer<typeof LoginSchema>) => {
-    mutation.mutate(data);
-  }
-
-  return {form, onSubmit};
-}
+  return { form };
+};
