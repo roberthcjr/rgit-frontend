8c25c32c7e8abeb701bd5973c2353ce36f0a911b
GarthTeixeira <garthur1998@gmail.com>
2025-05-24 10:07:37 -0300

    refactor(tool-form): isolate form in single-component

--- src/app/(private)/tools/components/insert-dialog.tsx
+++ src/app/(private)/tools/components/insert-dialog.tsx
@@ -1,25 +1,8 @@
 import { CustomDialog } from "@/components/custom-dialog";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/components/ui/form";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-
-import { Input } from "@/components/ui/input";
 import { CirclePlus } from "lucide-react";
 import { Dispatch, SetStateAction } from "react";
-import { statusSelectOptions } from "../model";
+import { ToolForm } from "./forms/tool-form";
 
 interface ImportToolProps {
   form: any;
@@ -46,81 +29,10 @@ export function InsertToolDialog({
       }
       title="Inserir nova ferramenta"
     >
-      <Form {...form} className="w-full">
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col gap-4 mt-4 w-96"
-        >
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormDescription>Nome da ferramenta</FormDescription>
-                <FormControl>
-                  <Input className="p-2" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="status"
-            render={({ field }) => (
-              <FormItem>
-                <FormDescription>Status da ferramenta</FormDescription>
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    {statusSelectOptions.map((option) => (
-                      <SelectItem key={option.value} value={option.value}>
-                        {option.label}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="brand.name"
-            render={({ field }) => (
-              <FormItem>
-                <FormDescription>Marca</FormDescription>
-                <FormControl>
-                  <Input className="p-2" {...field} />
-                </FormControl>
-
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="category.name"
-            render={({ field }) => (
-              <FormItem>
-                <FormDescription>Categoria da ferramenta</FormDescription>
-                <FormControl>
-                  <Input {...field} className="p-2" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Inserir</Button>
-        </form>
-      </Form>
+      <ToolForm
+        form={form}
+        onSubmit={onSubmit}
+      />
     </CustomDialog>
   );
 }
