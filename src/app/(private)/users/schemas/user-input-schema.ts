56f3ee552c4cded8db2ad240b1d2d99ef580e2d5
Robert Carvalho <robert.carvalho@educare.digital>
2025-05-31 14:12:43 -0300

    feat: set a min value to name

--- src/app/(private)/users/schemas/user-input-schema.ts
+++ src/app/(private)/users/schemas/user-input-schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const UserSchema = z.object({
-  name: z.string(),
+  name: z.string().min(6, "O nome do usuário deve ter pelo menos 6 caracteres"),
   surname: z.string().optional(),
   username: z.string(),
   password: z.string(),
